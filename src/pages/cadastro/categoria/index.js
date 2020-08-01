0ca2483161daf3344e8a5fbd464af2cda7573082
Joyce Ferreira <joyceferreira.automacao@gmail.com>
2020-08-01 11:02:57 -0500

    Add herokuapp as backand provider

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -29,7 +29,7 @@ function CadastroCategoria() {
     );
   }
   useEffect(() => {
-    const URL_TOP = 'http://localhost:8080/categorias';
+    const URL_TOP = 'https://devopsflix.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServitor) => {
         const resposta = await respostaDoServitor.json();
